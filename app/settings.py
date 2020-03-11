8d710c2f19ac012ac5c57fca0e6e2aae9e53f809
0xSeb <sebastien.marchal@protonmail.com>
2020-03-11 22:50:10 +0100

    Use uuid instead of auto incremented ids

--- app/settings.py
+++ app/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = [
     'bo.local'
 ]
 
+AUTH_USER_MODEL = 'app.User'
 
 # Application definition
 
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app'
 ]
 
 MIDDLEWARE = [
@@ -84,6 +86,7 @@ DATABASES = {
         'PASSWORD': os.getenv('POSTGRES_PASSWORD')
     }
 }
+
 
 
 # Password validation
