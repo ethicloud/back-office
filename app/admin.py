1ae4eee5f6f82705ffd5cadb6e24181b4066a984
TristanPinaudeau <tristan.pinaudeau@gmail.com>
2020-03-12 01:13:38 +0100

    feat: add models and working bo

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import *
 
-admin.site.register(User, UserAdmin)
+admin.site.register(User)
+admin.site.register(Service)
+admin.site.register(Organization)
+admin.site.register(Instance)
--- app/models.py
+++ app/models.py
@@ -3,5 +3,26 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+class Organization(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=254)
+    slug = models.CharField(max_length=254)
+
+class Service(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=254)
+
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE, null=True)
+
+class Instance(models.Model):
+    STATUS = (
+        ('U', 'Up'),
+        ('D', 'Down'),
+    )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    url = models.URLField()
+    status = models.CharField(max_length=1, choices=STATUS)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
